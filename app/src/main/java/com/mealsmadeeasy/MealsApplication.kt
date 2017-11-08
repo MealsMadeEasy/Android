d083f6f8adcc3735687950fc1581cdf6499aa6ca
Andrew Bailey <abailey960@gmail.com>
2017-11-08 17:14:01 -0500

    Setup JodaTime

--- app/src/main/java/com/mealsmadeeasy/MealsApplication.kt
+++ app/src/main/java/com/mealsmadeeasy/MealsApplication.kt
@@ -1,6 +1,13 @@
 package com.mealsmadeeasy
 
 import android.app.Application
+import net.danlew.android.joda.JodaTimeAndroid
 
 class MealsApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        JodaTimeAndroid.init(this)
+    }
+
 }
