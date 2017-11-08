f6d46d4befa68e1e3aae01398fba0984cf7d09ef
Andrew Bailey <abailey960@gmail.com>
2017-11-08 16:57:15 -0500

    Create initial model classes

---
+++ app/src/main/java/com/mealsmadeeasy/model/Ingredient.kt
@@ -0,0 +1,8 @@
+package com.mealsmadeeasy.model
+
+data class Ingredient (
+        val id: String,
+        val name: String,
+        val quantity: Int,
+        val unitName: String
+)
---
+++ app/src/main/java/com/mealsmadeeasy/model/Meal.kt
@@ -0,0 +1,8 @@
+package com.mealsmadeeasy.model
+
+data class Meal (
+        val id: String,
+        val name: String,
+        val description: String,
+        val thumbnailUrl: String?
+)
