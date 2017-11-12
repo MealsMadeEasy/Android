f374e1a7b6ef7c5c541a18c33687fc013fcd69fd
Andrew Bailey <abailey960@gmail.com>
2017-11-12 18:43:03 -0500

    Add method stubs

--- app/src/main/java/com/mealsmadeeasy/ui/home/glance/WeekAtAGlanceFragment.kt
+++ app/src/main/java/com/mealsmadeeasy/ui/home/glance/WeekAtAGlanceFragment.kt
@@ -1,6 +1,9 @@
 package com.mealsmadeeasy.ui.home.glance
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import com.mealsmadeeasy.MealsApplication
 import com.mealsmadeeasy.data.MealStore
 import com.mealsmadeeasy.ui.BaseFragment
@@ -17,6 +20,12 @@ class WeekAtAGlanceFragment : BaseFragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         MealsApplication.component(this).inject(this)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // TODO return the root view of the week at a glance screen here.
+        return super.onCreateView(inflater, container, savedInstanceState)
     }
 
 }
--- app/src/main/java/com/mealsmadeeasy/ui/home/grocery/GroceryListFragment.kt
+++ app/src/main/java/com/mealsmadeeasy/ui/home/grocery/GroceryListFragment.kt
@@ -1,6 +1,9 @@
 package com.mealsmadeeasy.ui.home.grocery
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import com.mealsmadeeasy.MealsApplication
 import com.mealsmadeeasy.data.MealStore
 import com.mealsmadeeasy.ui.BaseFragment
@@ -17,6 +20,12 @@ class GroceryListFragment : BaseFragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         MealsApplication.component(this).inject(this)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // TODO return the root view of the grocery list here.
+        return super.onCreateView(inflater, container, savedInstanceState)
     }
 
 }
