1573dfbfb2e6389a3dee227000e8a02835f99ff4
Alexandra Durso <adurso3@gatech.edu>
2018-01-20 22:27:26 -0500

    Create initial profile screen

---
+++ app/src/main/java/com/mealsmadeeasy/model/DietaryRestrictions.kt
@@ -0,0 +1,5 @@
+package com.mealsmadeeasy.model
+
+/**
+ * Created by Alexandra on 1/19/2018.
+ */
---
+++ app/src/main/java/com/mealsmadeeasy/ui/home/profile/ProfileFragment.kt
@@ -0,0 +1,5 @@
+package com.mealsmadeeasy.ui.home.profile
+
+/**
+ * Created by Alexandra on 1/20/2018.
+ */
