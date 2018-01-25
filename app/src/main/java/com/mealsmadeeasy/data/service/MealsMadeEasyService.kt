5f072e9989793fb814951e44cf13f209768ecd7f
Andrew Bailey <abailey960@gmail.com>
2018-01-24 22:22:50 -0500

    Implement UserProfile API communication

---
+++ app/src/main/java/com/mealsmadeeasy/data/service/MealsMadeEasyService.kt
@@ -0,0 +1,24 @@
+package com.mealsmadeeasy.data.service
+
+import com.mealsmadeeasy.model.UserProfile
+import io.reactivex.Single
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.POST
+
+interface MealsMadeEasyService {
+
+    @GET("user/profile")
+    fun getUserProfile(
+            @Header("Authorization") token: String
+    ): Single<Response<UserProfile>>
+
+    @POST("user/profile")
+    fun setUserProfile(
+            @Header("Authorization") token: String,
+            @Body profile: UserProfile
+    ): Single<Response<Unit>>
+
+}
